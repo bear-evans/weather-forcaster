ca0881c5ec3e51b9373794daedb1e60e9816010e
bear-evans <bear-evans@outlook.com>
2021-06-07 00:21:12 -0400

    Wireframing, basic api call function

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,54 @@
 // The core weather app
 // =========================================
 var weatherApp = (function() {
-    // Main internal logic goes here
-    return {
+    // Function to perform API call
 
+    function getWeather (loc) {
+        let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + "London" + "&appid=157647bc87fa3aa322f07fdde14674c8";
+
+        let response = $.ajax({url: apiUrl});
+
+        if (!response.status == 200) {
+            // Error handling here
+        } else {
+            console.log(apiUrl);
+            console.log(response);    
+        }
+
+    }
+    // Function to draw current conditions to current forecast
+
+    // Function to call and draw future conditions
+    return {
+        getWeather: getWeather
+    }
+})();
+
+// =========================================
+// Search Handler
+// -----------------------------------------
+// The Handles the search boxes and history
+// =========================================
+var locSearch = (function() {
+
+    // Function to create event listeners
+    function historyClick(event) {
+        event.preventDefault();
+    }
+
+    function srchClick(event) {
+        event.preventDefault();
+    }
+    // Function to load searches to memory
+
+    // Function to store searches in an array
+
+    // Function to clean searches and submit to the core app
+
+    // Function to create and populate list of searches
+    return {
+        historyClick: historyClick,
+        srchClick: srchClick
     }
 })();
 
@@ -25,3 +70,8 @@ var weatherApp = (function() {
 // Code that runs upon page load
 // =========================================
 
+$("#search-history").on('click', locSearch.historyClick);
+$("#search-btn").on('click', locSearch.srchClick);
+
+console.log("Fired!");
+weatherApp.getWeather();
