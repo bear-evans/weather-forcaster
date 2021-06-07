798a9ba0fb0471bfe42a11087879b2ae8081e23c
bear-evans <bear-evans@outlook.com>
2021-06-07 11:58:21 -0400

    Search history now saves and populates automatically

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,17 +16,20 @@ var weatherApp = (function() {
     // Function to perform API call
 
     function getWeather (loc) {
-        let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + "London" + "&appid=157647bc87fa3aa322f07fdde14674c8";
+        let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + loc + "&appid=157647bc87fa3aa322f07fdde14674c8";
 
-        let response = $.ajax({url: apiUrl});
+        fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    // An error occurred
+                    return false;
+                }
 
-        if (!response.status == 200) {
-            // Error handling here
-        } else {
-            console.log(apiUrl);
-            console.log(response);    
-        }
-
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
     }
     // Function to draw current conditions to current forecast
 
@@ -48,19 +51,54 @@ var locSearch = (function() {
         event.preventDefault();
     }
 
+    // Handles the search button clicks
     function srchClick(event) {
         event.preventDefault();
+
+        console.log("Clicky clicky");
+        let srch = $("#search-bar").val();
+
+        saveSearch(srch);
+        weatherApp.getWeather(srch);
     }
     // Function to load searches to memory
 
     // Function to store searches in an array
+    function saveSearch (loc) {
+        let history = [];
+        console.log($(".past-search"));
+        $(".past-search").each(function(i) {
+            history[i] = $(this).text();
+            console.log("history[" + i +"] = " + history[i]);
+        });
+        history.push(loc);
+        if (history.length > 10) {
+            history.shift();
+        };
 
+        localStorage.setItem("history", JSON.stringify(history));
+        populateHistory();
+    }
     // Function to clean searches and submit to the core app
 
     // Function to create and populate list of searches
+    function populateHistory() {
+        let history = JSON.parse(localStorage.getItem("history"));
+        
+        // Grab the container and clear it before populating it with fresh buttons
+        let hContainer = $("#search-history");
+        hContainer.empty();
+        for (var i = 0; i < history.length; i++) {
+            let j = $("<button class='past-search btn-success col-12'>" + history[i] + "</button>");
+            console.log(j);
+            hContainer.prepend(j);
+        }
+    }
+
     return {
         historyClick: historyClick,
-        srchClick: srchClick
+        srchClick: srchClick,
+        populateHistory: populateHistory
     }
 })();
 
@@ -73,5 +111,6 @@ var locSearch = (function() {
 $("#search-history").on('click', locSearch.historyClick);
 $("#search-btn").on('click', locSearch.srchClick);
 
-console.log("Fired!");
-weatherApp.getWeather();
+console.log("Begins!");
+
+locSearch.populateHistory();
